718ec3836ea0316301cd9f662474296b77339063
Matt Scandalis <scandalis.matthew@gmail.com>
2019-11-13 08:53:44 -0800

    make iteration more robust
    remove reliance on assumptions that the write piping wont start until the next tick in the event loop. there is no guarantee that all objects with a stream interface have the same underlying implementation

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 module.exports = function(stream, options) {
+	var consume, nextChunk, started = false, finished = false;
+
 	var writeable = new require('stream').Writable(options);
 
-	var process, cb, started = false, finished = false;
+	const hasChunk = function(cb) {
+		consume = (data) => {
+			cb(null, data);
+			nextChunk();
+		};
+
+		if (!started) {
+			stream.pipe(writeable);
+			started = true;
+		}
+	};
 
 	writeable._write = function(chunk, encoding, callback) {
-		process({
+		nextChunk = callback;
+		consume({
 			chunk: chunk,
 			encoding: encoding
 		});
-		cb = callback;
 	};
 
 	writeable.on('finish', function() {
 		finished = true;
 	});
 
-	const hasChunk = function(cb) {
-		process = (data) => {
-			cb(null, data);
-		};
-	};
-
 	return function*() {
 		if (finished) {
 			return null;
 		}
-
-		if (!started) {
-			stream.pipe(writeable);
-			started = true;
-		}
-
-		cb && cb();
 
 		var data = yield hasChunk;
 		return data;
